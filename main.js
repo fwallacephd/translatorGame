d10d0806523fe1a9514ff5e34cdda726be440b30
Faith Wallace <fwallacephd@gmail.com>
2018-03-28 13:41:20 -0400

    updated font css

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
- $(".word-button").on("click", function(){
+//Click on the word button to go into the display.
+$(".word-button").on("click", function(){
       $("#word-container").append(this);
+      //Add a space.
       $("#word-container").append(" ");
   });
